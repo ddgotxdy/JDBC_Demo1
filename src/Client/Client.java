c0ef47d4623a2c2ff4702fc7505c6a9af41b6e3b
ddgo <1812805089@qq.com>
2022-02-27 23:13:03 +0800

    修改了客户端

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -11,35 +11,64 @@ public class Client extends Thread {
     // 访问的ip地址
 //    private static String Servername = "182.92.216.59";
 
-    public static void main(String args[]) throws Exception {
+    /**
+     * 连接服务器
+     * @param args
+     */
+
+    public static void main(String args[]) {
+        Socket client = null;
         try {
 //            本地测试
             InetAddress inetAddress = InetAddress.getLocalHost();
-            Socket client = new Socket(inetAddress.getHostName(),port);
+            client = new Socket(inetAddress.getHostName(),port);
 //            远程服务器测试，需要配置内网穿透
 //            Socket client = new Socket(Servername,port);
+            interaction(client);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void interaction(Socket client) {
+        try {
+            System.out.println("=======================图书管理系统====================\n" +
+                    "1：");
+
             DataInputStream in = new DataInputStream(client.getInputStream());
             DataOutputStream out = new DataOutputStream(client.getOutputStream());
-            File file = null;
-            do {
-                String path = scanner.nextLine();
-                file = new File(path);
-                if(file.isFile()) break;
-                else System.out.println("Error File name or is directory");
-            }while(true);
-            FileInputStream fileInputStream = new FileInputStream(file);
 
-            out.writeUTF("submit");
-            out.writeUTF(file.getName());
-            int b;
+            int id = 0;
             do {
-                b = fileInputStream.read();
-                out.write(b);
-            }while(b != -1);
-            client.close();
+                id = in.readInt();
+                switch (id) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        break;
+                }
+            }while(id != 0);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static void register() {
+
+    }
+
 
 }
